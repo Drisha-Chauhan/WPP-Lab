10475467776eda3b4fac9a4e5964ab95dfa2ed10
Drisha Chauhan <chauhan.drisha4116@gmail.com>
2025-02-20 12:26:29 +0530

    Delete LAB 3

--- LAB 3/Lab 3-Q1.py
+++
@@ -1,9 +0,0 @@
-word= list(input("Enter a word: ")) #converted to list as strings are immutable
-
-for i in range (1,len(word),2):
-    word[i]= word[i].upper() #Every even position character is capitalised
-
-new_word= ''.join(word) #converted the list back to a string 
-print(new_word)
-    
-    
